3d2b8af2e21e0c33911ee97cd23096c3f144c8f7
punkygreg <guigui.v1999@gmail.com>
2024-02-19 17:58:51 -0500

    skeleton

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Guigs-Leaderboard
+
+1. THE GOAL
+
+The goal of the BETA version of this program is to keep track of win loss records within CODESMITH family for the various social games present at the office
+Three games will be the focus of the BETA: Mario Kart, Super Smash Bros, and of course, Ping Pong (aka Table Tennis). 
+This will help promote healthy competition and entice more students to play to see how they fare against their cohort mates.
+
+
+2. THE GAMES
+
+MARIO KART:
+
+SMASH BROS:
+
+PING PONG:
+
+3. METHODS
+
+4. MVP
+
+5. WHATS TO COME
---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+const controller = require('./controllers');
+
+
+//serve index file to the page, main page
+
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+});
+
+//serve scss file to html
+app.get('/styles.scss', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, './client/styles.scss'));
+});
+
+////////////////////////////////////
+//player cards req
+app.get('/cards', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../pages/PlayerCards.html'));
+});
+
+////////////////////////////////////
+//Ping Pong page req
+app.get('/pong', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../pages/PingPong.html'));
+});
+
+
+////////////////////////////////////
+//Mario Kart page req
+app.get('/mariokart', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../pages/MarioKart.html'));
+});
+
+
+////////////////////////////////////
+//Super Smash page req
+app.get('/supersmash', (req, res) => {
+  return res.status(200).sendFile(path.resolve(__dirname, '../pages/SuperSmash.html'));
+});
+
+
+////////////////////////////////////
+//404 error handler
+app.use('*', (req, res) => {
+    return res.status(404).sendFile(path.join(__dirname, '../404.html'));
+  });
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+  module.exports = app;
